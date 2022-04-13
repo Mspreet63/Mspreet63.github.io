bcc8ce1dde8fe2e91b452ad052b029a11b6156de
Mspreet63 <93096182+Mspreet63@users.noreply.github.com>
2022-04-13 00:30:50 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+  
+function check()
+{
+if(document.getElementById('hiring').checked)
+{
+document.getElementById('hiringu').style.display = "block";
+}
+else
+{
+document.getElementById('hiringu').style.display = "none";
+}
+return;
+}
